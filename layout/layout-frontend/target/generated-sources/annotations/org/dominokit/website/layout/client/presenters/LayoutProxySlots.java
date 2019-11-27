fe776f74c92be362950dd48dd6658a131c25c344
rjeeb <rafat.albarouki@gmail.com>
2019-11-27 21:04:32 +0200

    first home page draft

--- layout/layout-frontend/target/generated-sources/annotations/org/dominokit/website/layout/client/presenters/LayoutProxySlots.java
+++
@@ -1,7 +0,0 @@
-package org.dominokit.website.layout.client.presenters;
-
-import org.dominokit.domino.api.client.mvp.slots.IsSlot;
-
-public interface LayoutProxySlots {
-  IsSlot<?> getContentSlot();
-}
